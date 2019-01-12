be11f41df261780c7159cf59dabbe24d6b0a5bf9
ana-karoline-catunda <ana-karoline.catunda@hpe.com>
2019-01-12 15:24:20 -0300

    Adding first version for burger builder

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -395,6 +395,8 @@ module.exports = function(webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap,
+                modules:true,
+                localIdentName: '[name]__[local]__[hash:base64:5]',
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
@@ -410,7 +412,6 @@ module.exports = function(webpackEnv) {
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap,
                 modules: true,
-                localIdentName: '[name]__[local]__[hash:base64:5]',
                 getLocalIdent: getCSSModuleLocalIdent,
               }),
             },
