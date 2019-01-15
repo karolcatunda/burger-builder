661a85057e77b70409b3f144f328f67e6b73058b
ana-karoline-catunda <ana-karoline.catunda@hpe.com>
2019-01-14 21:57:27 -0300

    building a responsive burger builder application, improving the ingredients to be putted on the burger more responsible

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,24 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map( ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])].map((_, ingredientIndex) => {
+                return <BurgerIngredient key={ingredientKey + ingredientIndex} type={ingredientKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients!</p>;
+        }
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
