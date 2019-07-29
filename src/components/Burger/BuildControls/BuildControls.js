e45c321464f3e3a8ec759cf253aa1cd95613398e
Ana Karoline L. Catunda <ana-karoline.catunda@hpe.com>
2019-07-29 18:41:57 -0300

    Ending order summary implementation

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,9 +11,19 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>U$ {props.price.toFixed(2)}</strong></p>
         {controls.map(control => (
-            <BuildControl key={control.label} label={control.label}/>
+            <BuildControl 
+                key={control.label} 
+                label={control.label} 
+                added={() => props.ingredientAdded(control.type)} 
+                removed={() => props.ingredientRemoved(control.type)}
+                disabled={props.disabled[control.type]} />
         ))}
+        <button 
+        className={classes.OrderButton}
+        disabled={!props.purchasable}
+        onClick={props.ordered}>ORDER NOW</button>
     </div>
 )
 
